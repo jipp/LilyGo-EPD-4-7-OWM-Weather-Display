047308f9f9b5ec143ac5a21913e75f9209b4f1b3
Wolfgang Keller <wolfgang.keller@ericsson.com>
2022-05-17 14:01:00 +0200

    Update owm_credentials.h

--- owm_credentials.h
+++ owm_credentials.h
@@ -1,27 +1,27 @@
 const bool DebugDisplayUpdate = false;
 
 // Change to your WiFi credentials
-const char* ssid     = "Your WiFi SSID";
-const char* password = "Your PASSWORD";
+const char* ssid     = "wobilix";
+const char* password = "Eins2Drei4";
 
 // Use your own API key by signing up for a free developer account at https://openweathermap.org/
-String apikey       = "Your OWM API Key";                      // See: https://openweathermap.org/
+String apikey       = "58d310da332a79d5409a9d20900a63e1";                      // See: https://openweathermap.org/
 const char server[] = "api.openweathermap.org";
 //http://api.openweathermap.org/data/2.5/forecast?q=Melksham,UK&APPID=your_OWM_API_key&mode=json&units=metric&cnt=40
 //http://api.openweathermap.org/data/2.5/weather?q=Melksham,UK&APPID=your_OWM_API_key&mode=json&units=metric&cnt=1
 
 //Set your location according to OWM locations
-String City             = "Bath";                          // Your home city See: http://bulk.openweathermap.org/sample/
-String Latitude         = "51.38";                         // Latitude of your location in decimal degrees
-String Longitude        = "-2.36";                         // Longitude of your location in decimal degrees
-String Language         = "EN";                            // NOTE: Only the weather description is translated by OWM
+String City             = "Aachen";                          // Your home city See: http://bulk.openweathermap.org/sample/
+String Latitude         = "50.7753455";                         // Latitude of your location in decimal degrees
+String Longitude        = "6.0838868";                         // Longitude of your location in decimal degrees
+String Language         = "DE";                            // NOTE: Only the weather description is translated by OWM
                                                            // Examples: Arabic (AR) Czech (CZ) English (EN) Greek (EL) Persian(Farsi) (FA) Galician (GL) Hungarian (HU) Japanese (JA)
                                                            // Korean (KR) Latvian (LA) Lithuanian (LT) Macedonian (MK) Slovak (SK) Slovenian (SL) Vietnamese (VI)
 String Hemisphere       = "north";                         // or "south"  
 String Units            = "M";                             // Use 'M' for Metric or I for Imperial 
-const char* Timezone    = "GMT0BST,M3.5.0/01,M10.5.0/02";  // Choose your time zone from: https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv 
+//const char* Timezone    = "GMT0BST,M3.5.0/01,M10.5.0/02";  // Choose your time zone from: https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv 
                                                            // See below for examples
-const char* ntpServer   = "0.uk.pool.ntp.org";             // Or, choose a time server close to you, but in most cases it's best to use pool.ntp.org to find an NTP server
+const char* ntpServer   = "fritz.box";             // Or, choose a time server close to you, but in most cases it's best to use pool.ntp.org to find an NTP server
                                                            // then the NTP system decides e.g. 0.pool.ntp.org, 1.pool.ntp.org as the NTP syem tries to find  the closest available servers
                                                            // EU "0.europe.pool.ntp.org"
                                                            // US "0.north-america.pool.ntp.org"
@@ -31,7 +31,7 @@ int  daylightOffset_sec = 3600; // In the UK DST is +1hr or 3600-secs, other cou
 
 // Example time zones
 //const char* Timezone = "MET-1METDST,M3.5.0/01,M10.5.0/02"; // Most of Europe
-//const char* Timezone = "CET-1CEST,M3.5.0,M10.5.0/3";       // Central Europe
+const char* Timezone = "CET-1CEST,M3.5.0,M10.5.0/3";       // Central Europe
 //const char* Timezone = "EST-2METDST,M3.5.0/01,M10.5.0/02"; // Most of Europe
 //const char* Timezone = "EST5EDT,M3.2.0,M11.1.0";           // EST USA  
 //const char* Timezone = "CST6CDT,M3.2.0,M11.1.0";           // CST USA
